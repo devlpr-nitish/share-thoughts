b4aec8b68e873aa8be7416db511c5b34bbd4cffa
SSK <100ravsinghkarmwar@gmail.com>
2024-09-03 07:21:58 +0530

    feat: add CORS support to the backend

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
-import express from 'express';
+import express from "express";
 
-
-
-
-
+import cors from "cors";
 
 const server = express();
 
-server.get("/", (req,res,next)=>{
-    res.status(200).send("Hello welcome to Share Thoughts");
-})
+// Enable CORS for all requests
+server.use(cors());
 
-server.listen(3000, ()=>{
-    console.log("backend server is runnnig on port 3000");
-})
+server.get("/", (req, res, next) => {
+  res.status(200).send("Hello welcome to Share Thoughts");
+});
+
+server.listen(3000, () => {
+  console.log("backend server is runnnig on port 3000");
+});
+
